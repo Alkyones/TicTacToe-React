3dda8462945f639905b3d1839b4b71b198dfaf79
Ata Reenes <lifeonshort@gmail.com>
2022-09-02 13:12:36 +0300

    responsive style added

--- src/App.js
+++ src/App.js
@@ -39,6 +39,46 @@ function App() {
 
   }
 
+  function limitChange(event) {
+    const { name, value } = event.target
+    setScoreLimit(prevVal => ({
+      ...prevVal,
+      scorelimit: value
+    }))
+  }
+
+  const winSetValue = () => {
+    resetBoard()
+    setScoreLimit(prevVal => ({
+      ...prevVal,
+      scorelimit: 0}))
+
+    setScoreBoard(prevVal => ({...prevVal, xPlayerScore:0,oPlayerScore:0}))
+    setXPlayer(true);
+  }
+
+  const WinnerMessage = (props) => {
+    return(
+      <div className="App">
+        <div className="winner-message-container">
+          <h1>Winner is {props.winner}!</h1>
+          <input type='button' className='newGameButton' value='Start New Game!' onClick={winSetValue}></input>
+        </div>
+      </div>
+    )
+  }
+
+  const checkWin = (board) => {
+    for (let i = 0; i < win_states.length; i++) {
+      const [x, y, z] = win_states[i];
+
+      if (board[x] && board[x] === board[y] && board[y] === board[z]) {
+        setGameFinished(true); // declare game finished
+        return board[x];
+      }
+    }
+  };
+
   //assign value on click
   const handleBoxClick = (boxIndex) => {
     const updateBoard = board.map((value, index) => {
@@ -78,61 +118,21 @@ function App() {
 
   }
 
-  const checkWin = (board) => {
-    for (let i = 0; i < win_states.length; i++) {
-      const [x, y, z] = win_states[i];
-
-      if (board[x] && board[x] === board[y] && board[y] === board[z]) {
-        setGameFinished(true); // declare game finished
-        return board[x];
-      }
-    }
-  };
-
-
-  function limitChange(event) {
-    const { name, value } = event.target
-    setScoreLimit(prevVal => ({
-      ...prevVal,
-      scorelimit: value
-    }))
-  }
-
-  const winMessage = () => {
-    resetBoard()
-    setScoreLimit(prevVal => ({
-      ...prevVal,
-      scorelimit: 0}))
-
-    setScoreBoard(prevVal => ({...prevVal, xPlayerScore:0,oPlayerScore:0}))
-    setXPlayer(true);
-  }
-
-
-  const WinnerMessage = (props) => {
-    return(
-      <div className="App">
-        <div className="winner-message-container">
-          <h1>Winner is {props.winner}!</h1>
-          <input type='button' className='newGameButton' value='Start New Game!' onClick={winMessage}></input>
-        </div>
-      </div>
-    )
-  }
-
-  const RenderComps = () => {
+  // rendering function with conditionals
+  const RenderComponents = () => {
     if (scoreLimit.scorelimit === 0) {
+      const options = [
+        {value: '', label: 'Choose'}, {value: '1', label: '1'},
+        {value: '3', label: '3'},{value: '5', label: '5'},
+        {value: '7', label: '7'},{value: '9', label: '9'},]
       return (
         <div className="App">
           <div className='win-point-container'>
             <span>How many rounds you want to play ?</span>
             <select id='scorelimit' name='scorelimit' value={scoreLimit.scorelimit} onChange={limitChange} >
-              <option value='' selected>Choose</option>
-              <option value='1'>1</option>
-              <option value='3'>3</option>
-              <option value='5'>5</option>
-              <option value='7'>7</option>
-              <option value='9'>9</option>
+              {options.map((obj, index) => { return (
+                <option key={`${obj.label}_${index}`} value={obj.value}>{obj.label}</option>
+              )})}
             </select>
           </div>
         </div>
@@ -164,7 +164,7 @@ function App() {
   }
 
   return (
-    <RenderComps />
+    <RenderComponents />
   );
 }
 
